9ba5aff8952f258d56c7c03eacd4d79581c69781
Borislav Atanasov <120386696+borislavDci@users.noreply.github.com>
2024-02-10 16:14:24 +0100

    Update NavItem className to include md:flex-row

--- client/src/components/Navbar/NavItem.jsx
+++ client/src/components/Navbar/NavItem.jsx
@@ -7,7 +7,7 @@ function NavItem({ title, icon, pathname }) {
     <li className="px-2">
       <NavLink
         to={pathname}
-        className="hover:underline flex gap-3 items-center justify-center flex-row-reverse "
+        className="hover:underline flex gap-3 items-center justify-center flex-row-reverse md:flex-row "
       >
         {" "}
         <FontAwesomeIcon className="text-xl" icon={icon} /> <span>{title}</span>
